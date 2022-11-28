549c77a851a08e21b666b6c2b1ad31f9716daad5
Daniel Konecny <daniel.konecny@broadcom.com>
2022-11-28 23:46:30 +0100

    Polish mob instancing

--- src/main_scene.rs
+++ src/main_scene.rs
@@ -34,47 +34,28 @@ impl Main {
 
     #[method]
     fn on_mobtimer_timeout(&mut self, #[base] owner: &Node) {
-        // let mob_scene: Ref<KinematicBody, _> = instance_scene(&self.mob_scene);
         let mob_scene: Ref<KinematicBody, _> = utils::instance_scene(&self.mob_scene);
-        // let mob: mob::Mob = unsafe {
-        //     mob.try_cast::<mob::Mob>().expect("mob_scene should be Mob struct");
-        // };
         let mob_spawn_location = unsafe {
             owner
                 .get_node_as::<PathFollow>("SpawnPath/SpawnLocation")
                 .unwrap()
         };
-        // let mob_spawn_location = owner.get_node("path").expect("bla");
-                // .get_node_as::<PathFollow>("SpawnPath/SpawnLocation")
-                // .unwrap();
         let mut rng = rand::thread_rng();
         let offset: f64 = rng.gen();
-        mob_spawn_location.set_offset(offset);
+        mob_spawn_location.set_unit_offset(offset);
 
-        let player: TRef<KinematicBody> = unsafe { owner.get_node_as::<KinematicBody>("Player").unwrap() };
-        // let bla = Option;
-        // let player: TRef<KinematicBody> = unsafe { owner.get_node_as::<KinematicBody>("Player").ok().unwrap_or_else(||godot_print!("wtf??")) };
+        let player: TRef<KinematicBody> =
+            unsafe { owner.get_node_as::<KinematicBody>("Player").unwrap() };
         let player_position = player.translation();
 
-        let mob_scene = unsafe {mob_scene.into_shared().assume_safe()};
+        // add mob to scene and initialize it
+        let mob_scene = unsafe { mob_scene.into_shared().assume_safe() };
         owner.add_child(mob_scene, false);
-
         let mob = mob_scene.cast_instance::<mob::Mob>().unwrap();
+        mob.map_mut(|m, node| m.initialize(&node, mob_spawn_location.translation(), player_position))
+            .ok()
+            .unwrap_or_else(|| godot_print!("unable to get mob"));
 
-        // let b = mob::Mob::new(mob_scene.as_ref());
-        // b.initialize(owner, start_position, player_position);
-
-        // let mut mmob: mob::Mob = mob.clone();
-        // let mob = mob.claim();
-        // let b: &mob::Mob = mob.script().try_into().as_ref().ok().unwrap();
-        mob.map_mut(|x: &mut mob::Mob, mob_owner| x.initialize(&mob_owner, mob_spawn_location.translation(), player_position))
-        .ok()
-        .unwrap_or_else(|| godot_print!("unable to get mob"));
-        // mob.map(|x, mob_owner| x.initialize(&mob_owner, mob_spawn_location.translation(), player_position))
-        // .ok()
-        // .unwrap_or_else(|| godot_print!("unable to get mob"));
-        
-        // initialize(mob_spawn_location.translation(), player_position);
     }
 }
 
