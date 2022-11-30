b3a6a62259adc8f10337d37e56dd2412f5bbd8e6
Daniel Konecny <daniel.konecny@broadcom.com>
2022-11-30 23:29:43 +0100

    Gracefully handle dead player panic

--- src/main_scene.rs
+++ src/main_scene.rs
@@ -4,15 +4,12 @@ use rand::Rng;
 
 use crate::mob;
 use crate::utils;
-// use crate::player;
-// use std::f64::consts::PI;
 
 #[derive(NativeClass)]
 #[inherit(Node)]
 pub struct Main {
     #[property]
     mob_scene: Ref<PackedScene>,
-    // velocity: Vector3,
 }
 
 #[methods]
@@ -20,7 +17,6 @@ impl Main {
     fn new(_owner: &Node) -> Self {
         Main {
             mob_scene: PackedScene::new().into_shared(),
-            // velocity: Vector3::ZERO,
         }
     }
 
@@ -36,9 +32,13 @@ impl Main {
         let offset: f64 = rng.gen();
         mob_spawn_location.set_unit_offset(offset);
 
-        let player: TRef<KinematicBody> =
-            unsafe { owner.get_node_as::<KinematicBody>("Player").unwrap() };
-        let player_position = player.translation();
+        // handle player position even if it's not there (a.k.a. is dead!)
+        let player_position: Vector3;
+        let player =unsafe { owner.get_node_as::<KinematicBody>("Player") };
+        match player {
+            Some(p) => player_position = p.translation(),
+            None => player_position = Vector3 { x: rng.gen_range(-14.0..=14.0), y: 0.0, z: rng.gen_range(-14.0..=16.0) },
+        }
 
         // add mob to scene and initialize it
         let mob_scene = unsafe { mob_scene.into_shared().assume_safe() };
